4d805c369b9ad4f1e41cef218ade8f1993300fd1
garvitkumar854 <garvitkajot854@gmail.com>
2026-02-12 21:57:38 +0530

    Updated

--- 04_SettingUp_Database/src/db/db.js
+++ 04_SettingUp_Database/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-    // cluster uri: "mongodb+srv://Garvit:jc7DhOD4W1qcFBRJ@backend.amxg3j4.mongodb.net/"
+    // cluster uri: "<cluster_uri/database"
     // connect to the database
     await mongoose.connect("<your-mongodb-uri>")
     // use await = because of waiting to connect to the database, then only move to next line
